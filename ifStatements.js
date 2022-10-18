77f9011abd4e300dfe5f9b0d8974368bab2e2796
Katherine Blaine <blaine.katherine12@gmail.com>
2022-10-18 16:05:31 -0600

    Update ifStatements file response

--- ifStatements.js
+++ ifStatements.js
@@ -65,15 +65,15 @@
   var cupsOfFlour = 3;
   var hasSauce = true;
   
-  // if (cupsOfFlour === 1 && hasSauce === true) {
-  //   console.log("I cannot make pizza.");
-  // } else if (cupsOfFlour === 5 && hasSauce === false) {
-  //   console.log("I cannot make pizza");
-  // } else if (cupsOfFlour === 2 && hasSauce === true) {
-  //   console.log("I can make pizza!")
-  // } else if (cupsOfFlour === 3 && hasSauce === true) {
-  //   console.log("I can make pizza!")
-  // }
+  if (cupsOfFlour === 1 && hasSauce === true) {
+    console.log("I cannot make pizza.");
+  } else if (cupsOfFlour === 5 && hasSauce === false) {
+    console.log("I cannot make pizza");
+  } else if (cupsOfFlour === 2 && hasSauce === true) {
+    console.log("I can make pizza!")
+  } else if (cupsOfFlour === 3 && hasSauce === true) {
+    console.log("I can make pizza!")
+  }
   
   
   if (cupsOfFlour === 1 && hasSauce === true || cupsOfFlour === 5 && hasSauce === false) {
